999ef8a427d3573fbf8e9541d08f254ec9899e4a
GraceHyh06 <smilehyh@outlook.com>
2021-01-06 10:58:50 +1000

    Can get some html pages based on url

--- FirstApp.js
+++ FirstApp.js
@@ -1,14 +1,24 @@
 const http = require('http');
 const fs = require('fs')
-const fileName = './demoFile.html'
 const dt = require('./dateTimeModule');
+const url = require('url');
+const { runInNewContext } = require('vm');
 
 console.log('First App is running')
 http.createServer((req, res)=>{
-  fs.readFile(fileName, (err, data)=>{
+  var q = url.parse(req.url);
+
+  var fileName = './demoFile.html'
+
+  if(q.pathname !== '/')
+    fileName = '.'+q.pathname;
+
+    fs.readFile(fileName, (err, data)=>{
     res.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
-    //res.write("The date and time are currently: " + dt.myDateTime().toString());
-    res.write(data);
+    if(err)
+      res.write(err.message+' Error happened at '+ dt.myDateTime().toString())
+    else
+      res.write(data);
     res.end('');
     })
 }).listen(8080);
